5b5353f9eed0e701d6c6501e1e1285b7958d0027
lolosk <lwlosk@gmail.com>
2016-01-31 16:58:59 +0200

    Basic RADOS connection management

--- demo/utils/radosBindings.py
+++ demo/utils/radosBindings.py
@@ -1,43 +1,87 @@
 import rados
 import re
+import time
+import threading
+import signal
 
 CEPH_CONF = 'demo/utils/ceph.conf'
 KEYRING   = 'demo/utils/ceph.client.admin.keyring'
 POOL      = 'data'
-ioctx     = None
+TIMEOUT   = 2
+cluster   = None
 
-def connected(error_value=None):
+
+def _test_connection():
+    try:
+        test_conn = rados.Rados(conffile=CEPH_CONF, conf=dict(keyring=KEYRING))
+        test_conn.connect(timeout=TIMEOUT)
+        print "*** Connection OK ***"
+        return True
+    except:
+        print "*** Connection FAILED ***"
+        return False
+    finally:
+        try:
+            test_conn.shutdown()
+        except:
+            pass
+
+def _connect(): 
+    global cluster
+    while True:
+        if _test_connection():
+            if cluster is None:
+                try:
+                    new_cluster = rados.Rados(conffile=CEPH_CONF, conf=dict(keyring=KEYRING))
+                    new_cluster.connect(timeout=TIMEOUT)
+                    cluster = new_cluster
+                    print "*** Connection Established ***"
+                except:
+                    print "*** Could not establish connection ***"
+        else:
+            if cluster is not None:
+                try:
+                    cluster.shutdown()
+                finally:
+                    cluster = None
+                    print "*** Shut down previous connection ***"
+        time.sleep(20)
+            
+threading.Thread(target=_connect).start()
+
+def connected(ret_type):
     def decorator(f):
         def wrapped(*args, **kwargs):
-            global ioctx
             try:
-                cluster = rados.Rados(conffile=CEPH_CONF, 
-                                      conf=dict(keyring=KEYRING))
-                cluster.connect(timeout=2)
-                ioctx = cluster.open_ioctx(POOL)
-                return f(*args, **kwargs)
+                print "*** Submitting Query ***"
+                ret = f(*args, **kwargs)
+                print "*** Query succeeded ***"
+                return ret
             except:
-                return error_value
-            finally:
-                cluster.shutdown()
+                print "*** Query failed ***"
+                return ret_type()
         return wrapped
     return decorator
 
-@connected(error_value=[])
+@connected(ret_type=list)
 def get_object_list():
+    ioctx = cluster.open_ioctx(POOL)
     return [o.key for o in ioctx.list_objects()]
 
-@connected(error_value="")
+@connected(ret_type=str)
 def get_data(obj):
+    ioctx = cluster.open_ioctx(POOL)
     return ioctx.read(str(obj))
 
-@connected(error_value=False)
+@connected(ret_type=bool)
 def delete_object(obj):
+    ioctx = cluster.open_ioctx(POOL)
     ioctx.remove_object(str(obj))
     return True
 
-@connected(error_value=False)
+@connected(ret_type=bool)
 def store_object(name, data):
+    ioctx = cluster.open_ioctx(POOL)
     ioctx.write_full(str(name), str(data))
     return True
 
--- demo/utils/testrados.py
+++ demo/utils/testrados.py
@@ -1,9 +1,9 @@
 
 import rados
 
-cluster = rados.Rados(conffile = 'ceph.conf', conf = dict (keyring = 'ceph.client.admin.keyring'))
-cluster.connect()
+cluster = rados.Rados(conffile = 'ceph.conf', conf=dict(keyring='ceph.client.admin.keyring'))
+cluster.connect(timeout=2)
 ioctx = cluster.open_ioctx('data')
 print([o.key for o in ioctx.list_objects()])
-print ioctx.read('object-4')
+#print ioctx.read('object-4')
 
